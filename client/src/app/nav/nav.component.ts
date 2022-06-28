d8e965f0b18c9d1d0cf9a9898426a5259d247870
Julien <flynstone@live.ca>
2022-06-28 14:27:05 -0400

    added routes to the client

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -13,20 +13,24 @@ export class NavComponent implements OnInit {
   model: any = {};
 
   // Set account service to public to allow html template to have access.
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
+      // Redirect users to the members page after successfully logging in.
+      this.router.navigateByUrl('/members');
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);
     });
   }
 
   logout() {
     this.accountService.logout();
+    // Redirect users to the home page after they logout.
+    this.router.navigateByUrl('/');
   }
 }
